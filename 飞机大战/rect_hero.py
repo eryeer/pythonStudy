32b2e6212a412bfa74e5ee14ab372fe893cd11ec
eryeer <eryeer@163.com>
2018-11-29 23:47:55 +0800

    game

---
+++ 飞机大战/rect_hero.py
@@ -0,0 +1,6 @@
+import pygame
+
+hero_rect = pygame.Rect(100, 500, 120, 125)
+print("origin %d %d " % (hero_rect.x, hero_rect.y))
+print("size %d %d " % (hero_rect.width, hero_rect.height))
+print(hero_rect.size)
