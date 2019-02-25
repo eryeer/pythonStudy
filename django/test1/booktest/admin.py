3b516bbf77b09d93dcf993d94c1c3bd846b6399d
eryeer <eryeer@163.com>
2019-02-25 08:30:35 +0800

    finish django

--- django/test1/booktest/admin.py
+++ django/test1/booktest/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from booktest.models import BookInfo, HeroInfo
+
+
+# 自定义模型管理类
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'btitle', 'bpub_date']
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'bname', 'bcomment']
+
 
 # Register your models here.
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo)
--- django/test1/booktest/models.py
+++ django/test1/booktest/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
+
+# 一类
 # Create your models here.
 class BookInfo(models.Model):
-    pass
+    # 字符串并指定最大长度
+    btitle = models.CharField(max_length=20)
+    # 日期
+    bpub_date = models.DateField()
+
+    def __str__(self):
+        return self.btitle
+
+
+# 多类
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)
+    hgender = models.BooleanField(default=False)
+    hage = models.IntegerField()
+    hcomment = models.CharField(max_length=128)
+    hbook = models.ForeignKey('BookInfo')
+
+    def __str__(self):
+        return self.hname
